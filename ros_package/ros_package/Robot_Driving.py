870f56df32fa6d29d363773248186251f1110b90
jongchanjang <mawinerttt@gmail.com>
2024-05-27 11:33:02 +0900

    Feat: Driving action by robot_command , modify voice msg

---
+++ ros_package/ros_package/Robot_Driving.py
@@ -0,0 +1,275 @@
+import rclpy as rp
+from rclpy.node import Node
+from ros_package_msgs.srv import CommandString
+from ros_package_msgs.msg import RobotState
+from nav2_simple_commander.robot_navigator import BasicNavigator
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import TaskResult
+import threading
+import time
+
+class RobotDriver(Node):
+    def __init__(self):
+        super().__init__('Robot_Driver')
+
+        # 로봇 상태 나타내는 퍼블리셔 
+        self.robot_state_pose_publisher = self.create_publisher(RobotState, '/robot_state', 10)
+        
+        # Robot_Driving/robot_command 서비스를 받아오는 서비스 서버 생성
+        self.robot_command_server = self.create_service(CommandString, '/Robot_Driving/robot_command', self.robot_command_callback)
+        
+        # # 기본적으로 navigator를 초기화
+        # self.navigator = BasicNavigator()
+        # self.navigator.waitUntilNav2Active()
+
+        # 순찰 상태 관리
+        self.current_state = 'Patrolling'
+        self.patrolling = True
+        self.command_received = False
+
+        # 순찰 방향을 위한 플래그
+        self.forward_patrol = True
+
+        # 주기적으로 로봇 상태를 퍼블리시
+        self.state_check_timer = self.create_timer(1.0, self.publish_robot_state)
+
+        # 순찰 작업을 위한 스레드
+        self.patrol_thread = threading.Thread(target=self.patrol_work)
+        self.patrol_thread.start()
+
+    
+    def publish_robot_state(self):
+        msg = RobotState()
+        msg.command = self.current_state
+
+        self.robot_state_pose_publisher.publish(msg)
+        print(msg.command)
+        # self.get_logger().info('Published robot state: {}'.format(msg.command))
+
+
+    def patrol_work(self):
+        goal_poses = [
+            ((0.8, 0.8, 0.0), (0.0, 0.0, 0.0)),  # 첫 번째 목표 지점
+            ((1.3, 4.0, 0.0), (0.0, 0.0, 0.0)),  # 두 번째 목표 지점
+            ((6.0, 3.7, 0.0), (0.0, 0.0, 0.0)),  # 세 번째 목표 지점
+            ((6.0, 0.0, 0.0), (0.0, 0.0, 0.0)),  # 네 번째 목표 지점
+            ((10.0, 0.0, 0.0), (0.0, 0.0, 0.0)),  # 다섯 번째 목표 지점
+            ((10.0, 3.3, 0.0), (0.0, 0.0, 0.0)),  # 여섯 번째 목표 지점
+            ((15.0, 2.8, 0.0), (0.0, 0.0, 0.0)),  # 일곱 번째 목표 지점
+            ((15.0, 0.0, 0.0), (0.0, 0.0, 0.0)),  # 여덟 번째 목표 지점
+        ]
+
+        while rp.ok():
+            if self.patrolling and not self.command_received:
+                for i in range(len(goal_poses)):
+                    if not self.patrolling:
+                        break
+
+                    self.current_state = 'Patrolling'
+
+                    # index = i if self.forward_patrol else len(goal_poses) - 1 - i
+                    # goal_pose, orientation = goal_poses[index]
+
+                    # goal_x, goal_y, goal_z = goal_pose
+                    # goal_orientation_x, goal_orientation_y, goal_orientation_z = orientation
+
+                    # goal_pose_msg = PoseStamped()
+                    # goal_pose_msg.header.frame_id = 'map'
+                    # goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+                    # goal_pose_msg.pose.position.x = goal_x
+                    # goal_pose_msg.pose.position.y = goal_y
+                    # goal_pose_msg.pose.position.z = goal_z
+                    # goal_pose_msg.pose.orientation.x = goal_orientation_x
+                    # goal_pose_msg.pose.orientation.y = goal_orientation_y
+                    # goal_pose_msg.pose.orientation.z = goal_orientation_z
+                    # goal_pose_msg.pose.orientation.w = 1.0
+
+                    # self.get_logger().info('Navigating to goal: ({}, {}, {})'.format(goal_x, goal_y, goal_z))
+
+                    # # 목표 위치로 이동
+                    # self.navigator.goToPose(goal_pose_msg)
+
+                    # # 목표 지점에 도착하기를 기다림
+                    # while not self.navigator.isTaskComplete() and not self.command_received:
+                    #     time.sleep(0.1)
+
+                    # if self.command_received:
+                    #     self.navigator.cancelTask()
+                    #     break
+
+                    # result = self.navigator.getResult()
+                    
+                    # if result == TaskResult.SUCCEEDED:
+                    #     self.get_logger().info('Goal succeeded at ({}, {}, {})!'.format(goal_x, goal_y, goal_z))
+                    #     self.current_state = 'arrive at {}'.format(goal_pose)
+
+                    # elif result == TaskResult.CANCELED:
+                    #     self.get_logger().info('Goal was canceled at ({}, {}, {})!'.format(goal_x, goal_y, goal_z))
+
+                    # elif result == TaskResult.FAILED:
+                    #     self.get_logger().info('Goal failed at ({}, {}, {})!'.format(goal_x, goal_y, goal_z))
+                    
+                    # # 순찰 방향 업데이트
+                    # if self.forward_patrol and index == len(goal_poses) - 1:
+                    #     self.forward_patrol = False
+                    # elif not self.forward_patrol and index == 0:
+                    #     self.forward_patrol = True
+
+
+                    # 목표 지점 도착 후 3초 대기
+                    time.sleep(3)
+
+                    # 2번과 7번 웨이포인트에서는 커맨드를 무시하고 계속 진행
+                    if i not in [1, 6] and self.command_received:
+                        break
+
+
+    def robot_command_callback(self, request, response):
+        self.get_logger().info('Robot Command Server started')
+
+        self.command_received = True
+        
+        if request.command == "human_detect":
+            self.get_logger().info('Received human_detect')
+            response.success = True
+            response.message = 'human_detect'
+
+            self.patrolling = False
+            self.current_state = 'Human Detection'
+
+            self.handle_human_detect(request.description)
+ 
+
+        # elif request.command == "description":
+        #     self.get_logger().info('Received description')
+        #     response.success = True
+        #     response.message = 'description'
+
+        #     self.patrolling = False
+        #     self.current_state = 'Description'
+
+        #     self.handle_description()
+            
+
+        elif request.command == "guide":
+            self.get_logger().info('Received guide')
+            response.success = True
+            response.message = 'guide'
+
+            self.patrolling = False
+            self.current_state = 'Guiding'
+
+            self.handle_guide(request.description)
+
+            
+        elif request.command == "comeback":
+            self.get_logger().info('Received comeback')
+            response.success = True
+            response.message = 'comeback'
+
+            self.patrolling = True
+            self.current_state = 'Returning to Patrol'
+
+            self.comeback_to_patrol()
+
+        else:
+            self.get_logger().error('Received Unknown')
+            response.success = False
+            response.message = 'Unknown'
+
+        return response
+    
+
+    def handle_human_detect(self, description):
+        self.get_logger().info('human_detect.')
+        self.publish_robot_state()
+
+        # description 값을 이용해 목표 위치 설정
+        # goal_x, goal_y, goal_z = description[0,1,2]
+        # goal_orientation_x, goal_orientation_y, goal_orientation_z, goal_orientation_w = description[3,4,5,6]
+
+        # goal_pose_msg = PoseStamped()
+        # goal_pose_msg.header.frame_id = 'map'
+        # goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+        # goal_pose_msg.pose.position.x = goal_x
+        # goal_pose_msg.pose.position.y = goal_y
+        # goal_pose_msg.pose.position.z = goal_z
+        # goal_pose_msg.pose.orientation.x = goal_orientation_x
+        # goal_pose_msg.pose.orientation.y = goal_orientation_y
+        # goal_pose_msg.pose.orientation.z = goal_orientation_z
+        # goal_pose_msg.pose.orientation.w = goal_orientation_w
+
+        # self.navigator.goToPose(goal_pose_msg)
+
+        # while not self.navigator.isTaskComplete() and not self.command_received:
+        #     time.sleep(0.1)
+
+        # result = self.navigator.getResult()
+        
+        # if result == TaskResult.SUCCEEDED:
+        #     self.get_logger().info('Arrived at the description location!')
+
+        # elif result == TaskResult.CANCELED:
+        #     self.get_logger().info('Navigation to description location was canceled!')
+
+        # elif result == TaskResult.FAILED:
+        #     self.get_logger().info('Navigation to description location failed!')
+        
+
+    # def handle_description(self):
+    #     self.get_logger().info('Robot stopped for description.')
+
+
+    def handle_guide(self, description):
+        self.get_logger().info('Robot guiding to description location.')
+        self.publish_robot_state()
+
+        # # description 값을 이용해 목표 위치 설정
+        # goal_x, goal_y, goal_z = description[0,1,2]
+        # goal_orientation_x, goal_orientation_y, goal_orientation_z, goal_orientation_w = description[3,4,5,6]
+
+        # goal_pose_msg = PoseStamped()
+        # goal_pose_msg.header.frame_id = 'map'
+        # goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+        # goal_pose_msg.pose.position.x = goal_x
+        # goal_pose_msg.pose.position.y = goal_y
+        # goal_pose_msg.pose.position.z = goal_z
+        # goal_pose_msg.pose.orientation.x = goal_orientation_x
+        # goal_pose_msg.pose.orientation.y = goal_orientation_y
+        # goal_pose_msg.pose.orientation.z = goal_orientation_z
+        # goal_pose_msg.pose.orientation.w = goal_orientation_w
+
+        # self.navigator.goToPose(goal_pose_msg)
+
+        # while not self.navigator.isTaskComplete() and not self.command_received:
+        #     time.sleep(0.1)
+
+        # result = self.navigator.getResult()
+        
+        # if result == TaskResult.SUCCEEDED:
+        #     self.get_logger().info('Arrived at the description location!')
+
+        # elif result == TaskResult.CANCELED:
+        #     self.get_logger().info('Navigation to description location was canceled!')
+
+        # elif result == TaskResult.FAILED:
+        #     self.get_logger().info('Navigation to description location failed!')
+
+
+    def comeback_to_patrol(self):
+        self.patrolling = True
+        self.command_received = False
+
+
+def main(args=None):
+    rp.init(args=args)
+
+    robot_driving = RobotDriver()
+
+    rp.spin(robot_driving)
+
+    robot_driving.destroy_node()
+    rp.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ ros_package/ros_package/Robot_Service.py
@@ -0,0 +1,152 @@
+import rclpy as rp
+from rclpy.node import Node
+from ros_package_msgs.srv import CommandString
+import threading
+
+class RobotService(Node):
+    def __init__(self):
+        super().__init__('Robot_Service')
+
+        # Admin_Manager로 robot_command 서비스를 받아옴
+        self.robot_command_server = self.create_service(CommandString, '/robot_command', self.robot_command_callback)
+
+        # Robot_Driving로 robot_command 서비스를 보냄
+        self.robot_command_client = self.create_client(CommandString, 'Robot_Driving/robot_command')
+
+        # User_GUI로 robot_command 서비스를 보냄
+        self.robot_command_client_user = self.create_client(CommandString, 'User_GUI/robot_command')
+
+        # voice_recognize로 voice_start 서비스를 보냄
+        self.voice_signal_client = self.create_client(CommandString, '/voice_signal')
+
+
+    def robot_command_callback(self, request, response):
+        self.get_logger().info(f'Received command: {request.command}')
+
+        # 명령과 설명을 인자로 하여 스레드를 시작
+        thread = threading.Thread(target=self.working_order, args=(request.command, request.description))
+        thread.start()
+
+        response.success = True
+        response.message = f'Command {request.command} received and being processed'
+
+        return response
+    
+
+    def working_order(self, command, description):
+        if command == "human_detect":
+            self.get_logger().info('Processing human_detect')
+            self.send_robot_command(command, description)  # Robot_Driving: 작품설명 위치이동
+            self.send_robot_command_user(command, description)  # GUI음성: 무엇을 도와드릴까요?
+            self.send_voice_start_command('voice_start')  # voice_recog: 음성열기
+        
+        elif command == "description":
+            self.get_logger().info('Processing description')
+            # self.send_robot_command(command, description)  # Robot_Driving: 제자리 위치
+            self.send_robot_command_user(command, description)  # GUI음성: 작품설명..., 또다른 도움이 필요하십니까?
+            self.send_voice_start_command('voice_start')  # voice_recog: 음성열기
+
+        elif command == "guide":
+            self.get_logger().info('Processing guide')
+            self.send_robot_command_user(command, description)  # GUI음성: ~로 길안내 해드리겠습니다.
+            self.send_robot_command(command, description)  # Robot_Driving: 작품으로 길안내
+            self.send_robot_command_user_2(command, description)  # GUI음성: 또다른 도움이 필요하십니까?
+            self.send_voice_start_command('voice_start')  # voice_recog: 음성열기
+
+        elif command == "comeback":
+            self.get_logger().info('Processing comeback')
+            self.send_robot_command_user(command, description)  # GUI음성: 편안한 관람되십시오
+            self.send_robot_command(command, description)  # Robot_Driving: 작품설명 위치이동
+
+        else:
+            self.get_logger().error('Unknown command received')
+        
+
+    def send_robot_command(self, command: str, description: str = ""):
+        if not self.robot_command_client.wait_for_service(timeout_sec=5.0):
+            self.get_logger().error('Robot_command service not available')
+            return
+
+        request = CommandString.Request()
+        request.command = command
+        request.description = description
+
+        future = self.robot_command_client.call(request)
+
+        response = future
+
+        if response.success:
+            self.get_logger().info('Robot_command executed successfully (Robot to Driving): %s' % response.message)
+        else:
+            self.get_logger().error('Failed to execute User_command (Robot to Driving) : %s' % response.message)
+
+
+    def send_robot_command_user(self, command: str, description: str = ""):
+        if not self.robot_command_client_user.wait_for_service(timeout_sec=5.0):
+            self.get_logger().error('Robot_command service not available')
+            return
+
+        request = CommandString.Request()
+        request.command = command
+        request.description = description
+
+        future = self.robot_command_client_user.call(request)
+
+        response = future
+
+        if response.success:
+            self.get_logger().info('Robot_command executed successfully (Robot to User): %s' % response.message)
+        else:
+            self.get_logger().error('Failed to execute User_command (Robot to User) : %s' % response.message)
+
+
+    def send_robot_command_user_2(self, command: str, description: str = ""):
+        if not self.robot_command_client_user.wait_for_service(timeout_sec=5.0):
+            self.get_logger().error('Robot_command service not available')
+            return
+
+        request = CommandString.Request()
+        request.command = "re"
+        request.description = "또다른 도움이 필요하십니까?"
+
+        future = self.robot_command_client_user.call(request)
+
+        response = future
+
+        if response.success:
+            self.get_logger().info('Robot_command executed successfully (Robot to User): %s' % response.message)
+        else:
+            self.get_logger().error('Failed to execute User_command (Robot to User) : %s' % response.message)
+
+
+    def send_voice_start_command(self, command: str, description: str = ""):
+        if not self.voice_signal_client.wait_for_service(timeout_sec=5.0):
+            self.get_logger().error('Voice_signal service not available')
+            return
+
+        request = CommandString.Request()
+        request.command = command
+        request.description = description
+
+        future = self.voice_signal_client.call(request)
+
+        response = future
+
+        if response.success:
+            self.get_logger().info('Robot_command executed successfully (Robot to Voice): %s' % response.message)
+        else:
+            self.get_logger().error('Failed to execute User_command (Robot to Voice) : %s' % response.mess)
+
+
+def main(args=None):
+    rp.init(args=args)
+
+    robot_service = RobotService()
+
+    rp.spin(robot_service)
+
+    robot_service.destroy_node()
+    rp.shutdown()
+
+if __name__ == '__main__':
+    main()
