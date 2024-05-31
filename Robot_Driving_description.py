b211e5a54c0272ca5a63fcf1697e1d6536ad1962
YoonHJ97 <xxbb96@gmail.com>
2024-05-31 11:04:21 +0900

    modify Robot_Driving_description.py

--- Robot_Driving_description.py
+++ Robot_Driving_description.py
@@ -1,13 +1,12 @@
 import rclpy as rp
 from rclpy.node import Node
-from rclpy.duration import Duration
-from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from ros_package_msgs.srv import CommandString
 from ros_package_msgs.msg import RobotState
+from nav2_simple_commander.robot_navigator import BasicNavigator
 from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import TaskResult
 import threading
-import time , asyncio
-import routes
+import time
 
 
 class RobotDriver(Node):
@@ -19,210 +18,377 @@ class RobotDriver(Node):
         
         # Robot_Driving/robot_command 서비스를 받아오는 서비스 서버 생성
         self.robot_command_server = self.create_service(CommandString, '/Robot_Driving/robot_command', self.robot_command_callback)
-        
-        # 기본적으로 navigator를 초기화
+
+        # /patrol_command 서비스를 받아오는 서비스 서버 생성
+        self.patrol_command_server = self.create_service(CommandString, '/patrol_command', self.patrol_command_callback)
+            
+        # # 기본적으로 navigator를 초기화
         self.navigator = BasicNavigator()
         self.navigator.waitUntilNav2Active()
 
-        # 순찰 상태 관리 Patrolling
-        self.current_state = 'arrive'
-        self.patrolling = True
+        # 순찰 상태 관리
+        self.current_state = 'Start'
         self.command_received = False
-        self.current_point = 0
+        self.command_received = False
 
         # 순찰 방향을 위한 플래그
         self.forward_patrol = True
 
-        # 주기적으로 로봇 상태를 퍼블리시
-        self.state_check_timer = self.create_timer(1.0, self.publish_robot_state)
+        # 목표 지점의 인덱스 초기화
+        self.index = 0
 
-        # 순찰 경로 정의
-        self.route_forward = routes.route_forward
+        # 웨이포인트 , 작품설명위치
+        self.goal_poses = [
+            ((0.8, 0.8, 0.0), (0.0, 0.0, 0.0)),  # 첫 번째 목표 지점
+            ((1.3, 4.0, 0.0), (0.0, 0.0, 0.0)),  # 두 번째 목표 지점
+            ((6.0, 3.7, 0.0), (0.0, 0.0, 0.0)),  # 세 번째 목표 지점
+            ((6.0, 0.0, 0.0), (0.0, 0.0, 0.0)),  # 네 번째 목표 지점
+            ((10.0, 0.0, 0.0), (0.0, 0.0, 0.0)),  # 다섯 번째 목표 지점
+            ((10.0, 3.3, 0.0), (0.0, 0.0, 0.0)),  # 여섯 번째 목표 지점
+            ((15.0, 2.8, 0.0), (0.0, 0.0, 0.0)),  # 일곱 번째 목표 지점
+            ((15.0, 0.0, 0.0), (0.0, 0.0, 0.0)),  # 여덟 번째 목표 지점
+        ]
 
-        self.route_reverse = routes.route_reverse
-        
-        self.route_forward_segments = routes.route_forward_segments
-        
-        self.route_reverse_segments = routes.route_reverse_segments
-        
-        self.route_forward_description = routes.route_forward_description
+        self.goal_poses_art = ['피라미드','2','나폴레옹','스핑크스','오벨리스크','람세스','7','오시리스'] # 작품위치에 따른 작품명
+        self.current_art = None
 
-        self.route_resverse_description = routes.route_forward_description
+        self.point = [
+            ((2.2,1.6,0.0),(0.0,0.0,0.0,0.0)), # 첫번째 목표 지점의 작품설명위치
+            ((0.0,0.0,0.0),(0.0,0.0,0.0,0.0)), # 두번째 목표 지점의 작품설명위치
+            ((7.5,2.0,0.0),(0.0,0.0,0.0,0.0)), # 세번째 목표 지점의 작품설명위치
+            ((4.6,1.8,0.0),(0.0,0.0,0.0,0.0)), # 네번째 목표 지점의 작품설명위치
+            ((12.0,1.6,0.0),(0.0,0.0,0.0,0.0)), # 다섯번째 목표 지점의 작품설명위치
+            ((9.0,1.2,0.0),(0.0,0.0,0.0,0.0)), # 여섯번째 목표 지점의 작품설명위치
+            ((0.0,0.0,0.0),(0.0,0.0,0.0,0.0)), # 일곱번째 목표 지점의 작품설명위치
+            ((13.6,1.2,0.0),(0.0,0.0,0.0,0.0)), # 여덟번째 목표 지점의 작품설명위치
+        ]
         
-        
-        self.set_initial_pose(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0)
+        # 스타트 신호
+        self.publish_robot_state()
 
+        # 1번 웨이포인트로 이동
         self.patrol_work()
-    
-    def set_goal_pose(self, x, y, z, qx, qy, qz, qw):
-        goal_pose = PoseStamped()
-        goal_pose.header.frame_id = 'map'
-        goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        goal_pose.pose.position.x = x
-        goal_pose.pose.position.y = y
-        goal_pose.pose.position.z = z
-        goal_pose.pose.orientation.x = qx
-        goal_pose.pose.orientation.y = qy
-        goal_pose.pose.orientation.z = qz
-        goal_pose.pose.orientation.w = qw
-        return goal_pose
-    
-    def set_initial_pose(self, x, y, z, qx, qy, qz, qw):
-        initial_pose = PoseStamped()
-        initial_pose.header.frame_id = 'map'
-        initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        initial_pose.pose.position.x = x
-        initial_pose.pose.position.y = y
-        initial_pose.pose.position.z = z
-        initial_pose.pose.orientation.x = qx
-        initial_pose.pose.orientation.y = qy
-        initial_pose.pose.orientation.z = qz
-        initial_pose.pose.orientation.w = qw
-        self.navigator.setInitialPose(initial_pose)
 
-    def go_to_1(self):
-        self.follow_route_segment(0)
+        # 멈춤상태 신호
+        self.publish_robot_state()
 
-    def follow_route_segment(self, segment_index, max_retries=3):
-        current_route = self.route_forward_segments[segment_index]
-        for pt in current_route:
-            goal_pose = self.set_goal_pose(*pt)
-            self.navigator.goToPose(goal_pose)
-            retries = 0
-
-            while not self.navigator.isTaskComplete():
-                feedback = self.navigator.getFeedback()
-                if feedback:
-                    self.get_logger().info('Distance remaining: {:.2f}'.format(feedback.distance_remaining))
-
-            result = self.navigator.getResult()
-            if result == TaskResult.SUCCEEDED:
-                self.get_logger().info('Reached goal: ({}, {})'.format(pt[0], pt[1]))
-                self.current_point = segment_index
-            elif result == TaskResult.CANCELED:
-                self.get_logger().info('Goal was canceled, exiting.')
-                return
-            elif result == TaskResult.FAILED:
-                retries += 1
-                if retries >= max_retries:
-                    self.get_logger().info('Failed to reach goal: ({}, {}), max retries reached, exiting.'.format(pt[0], pt[1]))
-                    break
-                self.get_logger().info('Failed to reach goal: ({}, {}), retrying...'.format(pt[0], pt[1]))
-
-                            
-    def patrol_work(self):
-        routes = [self.route_forward, self.route_reverse]
-        current_route_index = 0
-
-        while rp.ok():
-            if self.patrolling and not self.command_received:
-                current_route = routes[current_route_index]
-                for i, pt in enumerate(current_route):
-                    if self.command_received:
-                        break
-                    goal_pose = self.set_goal_pose(*pt)
-                    self.navigator.goToPose(goal_pose)
-                    while not self.navigator.isTaskComplete():
-                        feedback = self.navigator.getFeedback()
-                        if feedback:
-                            self.get_logger().info('Distance remaining: {:.2f}'.format(feedback.distance_remaining))
-
-                    result = self.navigator.getResult()
-                    if result == TaskResult.SUCCEEDED:
-                        self.get_logger().info('Reached goal: ({}, {})'.format(pt[0], pt[1]))
-
-                        # human_detect 상태 확인 및 description_point로 이동
-                        if self.current_state == 'Human Detection':
-                            description_pose = self.get_description_pose(current_route_index, i)
-                            self.navigator.goToPose(description_pose)
-                            while not self.navigator.isTaskComplete():
-                                pass  # 대기
-
-                            self.get_logger().info('Reached description point')
-                            self.current_state = 'arrive'
-                            self.command_received = False  # Reset command_received 상태
-                            self.patrolling = True  # 다시 순찰 시작
-                    elif result == TaskResult.CANCELED:
-                        self.get_logger().info('Goal was canceled, exiting patrol.')
-                        return
-                    elif result == TaskResult.FAILED:
-                        self.get_logger().info('Failed to reach goal: ({}, {}), retrying...'.format(pt[0], pt[1]))
-                        break
-
-                current_route_index = (current_route_index + 1) % 2
 
     def publish_robot_state(self):
         msg = RobotState()
         msg.command = self.current_state
+
         self.robot_state_pose_publisher.publish(msg)
+
+
+    def patrol_work(self):
+        self.current_state = 'Patrolling'
+
+        self.publish_robot_state()
+
+        # 인덱스가 리스트 범위를 벗어나지 않도록 체크
+        if self.index < 0:
+            self.index = 0
+            self.forward_patrol = True
+        elif self.index >= len(self.goal_poses):
+            self.index = len(self.goal_poses) - 1
+            self.forward_patrol = False
+
+        goal_pose, orientation = self.goal_poses[self.index]
+
+        goal_x, goal_y, goal_z = goal_pose
+        goal_orientation_x, goal_orientation_y, goal_orientation_z = orientation
+
+        goal_pose_msg = PoseStamped()
+        goal_pose_msg.header.frame_id = 'map'
+        goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+        goal_pose_msg.pose.position.x = goal_x
+        goal_pose_msg.pose.position.y = goal_y
+        goal_pose_msg.pose.position.z = goal_z
+        goal_pose_msg.pose.orientation.x = goal_orientation_x
+        goal_pose_msg.pose.orientation.y = goal_orientation_y
+        goal_pose_msg.pose.orientation.z = goal_orientation_z
+        goal_pose_msg.pose.orientation.w = 1.0
+
+
+        self.get_logger().info('Navigating to goal: ({}, {}, {})'.format(goal_x, goal_y, goal_z))
+
+        # 목표 위치로 이동
+        self.navigator.goToPose(goal_pose_msg)
+
+        start_time = time.time()
+        timeout = 20  # 20초 타임아웃
+
+        while not self.navigator.isTaskComplete():
+            if time.time() - start_time > timeout:
+                self.navigator.cancelTask()
+                self.get_logger().warn('Navigation to goal timed out!')
+                break
+            time.sleep(0.1)
+
+        result = self.navigator.getResult()
         
+        if result == TaskResult.SUCCEEDED:
+            self.get_logger().info('Goal succeeded at ({}, {}, {})!'.format(goal_x, goal_y, goal_z))
+
+        elif result == TaskResult.CANCELED:
+            self.get_logger().info('Goal was canceled at ({}, {}, {})!'.format(goal_x, goal_y, goal_z))
+
+        elif result == TaskResult.FAILED:
+            self.get_logger().info('Goal failed at ({}, {}, {})!'.format(goal_x, goal_y, goal_z))
+
+
+        if self.index in [1, 6]:
+            self.current_state = 'not item at {}'.format(self.goal_poses_art[self.index])
+        else:
+            self.current_state = 'arrive at {}'.format(self.goal_poses_art[self.index])
+
+        self.current_art = self.goal_poses_art[self.index]
+
+
+        # 현재 목표 지점 인덱스 업데이트
+        if self.forward_patrol:
+            self.index += 1
+        else:
+            self.index -= 1
+
+
+    def patrol_command_callback(self, request, response):
+
+        self.get_logger().info('Patrol Command Server started')
+
+        if request.command == "patrol":
+            self.get_logger().info('Received patrol')
+            response.success = True
+            response.message = 'patrol'   
+
+            self.current_state = 'Patrolling'
+
+            self.publish_robot_state()
+        
+            self.patrol_work()
+
+            self.publish_robot_state()
+
+        return response
+
+
     def robot_command_callback(self, request, response):
+        
         self.get_logger().info('Robot Command Server started')
 
-        self.command_received = True
-        
         if request.command == "human_detect":
             self.get_logger().info('Received human_detect')
             response.success = True
-            response.message = 'human_detect'
+            response.message = 'human_detect'   
 
-            self.patrolling = False
             self.current_state = 'Human Detection'
+
+            self.publish_robot_state()
+
+            self.handle_human_detect()
+
 
         elif request.command == "description":
             self.get_logger().info('Received description')
             response.success = True
             response.message = 'description'
 
-            self.patrolling = False
             self.current_state = 'Description'
 
-            self.handle_description()
-            
+            self.publish_robot_state()
+
 
         elif request.command == "guide":
             self.get_logger().info('Received guide')
             response.success = True
             response.message = 'guide'
 
-            self.patrolling = False
             self.current_state = 'Guiding'
 
+            self.publish_robot_state()
+
             self.handle_guide(request.description)
-                        
+
         elif request.command == "comeback":
             self.get_logger().info('Received comeback')
             response.success = True
             response.message = 'comeback'
 
-            self.patrolling = True
-            self.current_state = 'Returning to Patrol'
+            self.current_state = 'comeback_to_patrol'
+
+            self.publish_robot_state()
 
             self.comeback_to_patrol()
+
+            self.patrol_work()
+
+            self.publish_robot_state()
 
         else:
             self.get_logger().error('Received Unknown')
             response.success = False
             response.message = 'Unknown'
 
+
         return response
     
 
     def handle_human_detect(self):
         self.get_logger().info('human_detect.')
-        time.sleep(10)
-        print('ok')
+
+        self.art_index = self.goal_poses_art.index(self.current_art) #현재 작품이 몇번째인지
+
+        self.descript_point = self.point[self.art_index] # i를 받은 작품설명 위치
+
+        goal_pose , orientation = self.descript_point # 작품설명위치의 위치와 자세
+
+        goal_x, goal_y, goal_z = goal_pose
+        goal_orientation_x, goal_orientation_y, goal_orientation_z, goal_orientation_w = orientation
+
+        goal_pose_msg = PoseStamped()
+        goal_pose_msg.header.frame_id = 'map'
+        goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+        goal_pose_msg.pose.position.x = goal_x
+        goal_pose_msg.pose.position.y = goal_y
+        goal_pose_msg.pose.position.z = goal_z
+        goal_pose_msg.pose.orientation.x = goal_orientation_x
+        goal_pose_msg.pose.orientation.y = goal_orientation_y
+        goal_pose_msg.pose.orientation.z = goal_orientation_z
+        goal_pose_msg.pose.orientation.w = goal_orientation_w
+
+        self.get_logger().info('Navigating to goal: ({}, {}, {})'.format(goal_x, goal_y, goal_z))
+
+        # 목표 위치로 이동
+        self.navigator.goToPose(goal_pose_msg)
+
+        start_time = time.time()
+        timeout = 12  # 12초 타임아웃
+
+        while not self.navigator.isTaskComplete():
+            if time.time() - start_time > timeout:
+                self.navigator.cancelTask()
+                self.get_logger().warn('Navigation to goal timed out!')
+                break
+            time.sleep(0.1)
+
+        result = self.navigator.getResult()
         
-    def handle_description(self):
-        self.get_logger().info('Robot stopped for description.')
+        if result == TaskResult.SUCCEEDED:
+            self.get_logger().info('Arrived at the  location!')
+
+        elif result == TaskResult.CANCELED:
+            self.get_logger().info('Navigation to location was canceled!')
+
+        elif result == TaskResult.FAILED:
+            self.get_logger().info('Navigation to location failed!')
+        
 
     def handle_guide(self, description):
-        self.get_logger().info('Robot guiding.')
+        self.get_logger().info('Robot guiding to description location.')
+
+        self.art_index = self.goal_poses_art.index(description) #작품명이 몇번째인지 확인
+
+        self.descript_point = self.point[self.art_index] # i를 받은 작품설명 위치
+
+        goal_pose , orientation = self.descript_point # 작품설명위치의 위치와 자세
+
+        goal_x, goal_y, goal_z = goal_pose
+        goal_orientation_x, goal_orientation_y, goal_orientation_z, goal_orientation_w = orientation
+
+        goal_pose_msg = PoseStamped()
+        goal_pose_msg.header.frame_id = 'map'
+        goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+        goal_pose_msg.pose.position.x = goal_x
+        goal_pose_msg.pose.position.y = goal_y
+        goal_pose_msg.pose.position.z = goal_z
+        goal_pose_msg.pose.orientation.x = goal_orientation_x
+        goal_pose_msg.pose.orientation.y = goal_orientation_y
+        goal_pose_msg.pose.orientation.z = goal_orientation_z
+        goal_pose_msg.pose.orientation.w = goal_orientation_w
+
+        self.navigator.goToPose(goal_pose_msg)
+
+        start_time = time.time()
+        timeout = 90  # 90초 타임아웃
+
+        while not self.navigator.isTaskComplete():
+            if time.time() - start_time > timeout:
+                self.navigator.cancelTask()
+                self.get_logger().warn('Navigation to goal timed out!')
+                break
+            time.sleep(0.1)
+
+        result = self.navigator.getResult()
+        
+        if result == TaskResult.SUCCEEDED:
+            self.get_logger().info('Arrived at the location!')
+
+        elif result == TaskResult.CANCELED:
+            self.get_logger().info('Navigation to location was canceled!')
+
+        elif result == TaskResult.FAILED:
+            self.get_logger().info('Navigation to location failed!')
+
+        self.current_state = 'guide at {}'.format(description)
+
+        self.publish_robot_state()
+
 
     def comeback_to_patrol(self):
-        self.patrolling = True
-        self.current_state = 'Patrolling'
-        # self.patrol_work()
+
+        # 가까운 웨이포인트로 이동
+        goal_pose, orientation = self.goal_poses[self.art_index]
+
+        goal_x, goal_y, goal_z = goal_pose
+        goal_orientation_x, goal_orientation_y, goal_orientation_z = orientation
+
+        goal_pose_msg = PoseStamped()
+        goal_pose_msg.header.frame_id = 'map'
+        goal_pose_msg.header.stamp = self.get_clock().now().to_msg()
+        goal_pose_msg.pose.position.x = goal_x
+        goal_pose_msg.pose.position.y = goal_y
+        goal_pose_msg.pose.position.z = goal_z
+        goal_pose_msg.pose.orientation.x = goal_orientation_x
+        goal_pose_msg.pose.orientation.y = goal_orientation_y
+        goal_pose_msg.pose.orientation.z = goal_orientation_z
+        goal_pose_msg.pose.orientation.w = 1.0
+
+        self.navigator.goToPose(goal_pose_msg)
+
+        start_time = time.time()
+        timeout = 12  # 12초 타임아웃
+
+        while not self.navigator.isTaskComplete():
+            if time.time() - start_time > timeout:
+                self.navigator.cancelTask()
+                self.get_logger().warn('Navigation to goal timed out!')
+                break
+            time.sleep(0.1)
+
+        result = self.navigator.getResult()
+        
+        if result == TaskResult.SUCCEEDED:
+            self.get_logger().info('Arrived at the description location!')
+
+        elif result == TaskResult.CANCELED:
+            self.get_logger().info('Navigation to description location was canceled!')
+
+        elif result == TaskResult.FAILED:
+            self.get_logger().info('Navigation to description location failed!')
+
+        
+        # 다음 웨이포인트 설정
+        if self.art_index in [0, 1, 2, 3]:
+            self.index = self.art_index + 1
+        else:
+            self.index = self.art_index - 1
+
+
+        #밑의 4개 점이면 위로향하게 , 위의 4개 점이면 아래로 향하게
+        if self.art_index in [0, 1, 2, 3]:
+            self.forward_patrol = True
+        else:
+            self.forward_patrol = False
 
 
 def main(args=None):
