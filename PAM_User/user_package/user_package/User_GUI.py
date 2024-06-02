7d8a4fca6181e8cdb3bfce6555c98777ba8a74e8
DJ_Y <djy404079@gmail.com>
2024-06-03 01:29:05 +0900

    Refactoring User_GUI.py
    중복 코드 제거 및 비동기화

--- PAM_User/user_package/user_package/User_GUI.py
+++ PAM_User/user_package/user_package/User_GUI.py
@@ -13,15 +13,13 @@ import cv2
 import numpy as np
 from gtts import gTTS
 import pygame
-
+import threading
 
 class Ros2PyQtApp(QDialog):
-    # ROS2에서 수신한 데이터를 업데이트하는 신호 정의
     update_image_signal = pyqtSignal(np.ndarray)
 
     def __init__(self):
         super().__init__()
-        
         uic.loadUi("/home/hj/amr_ws/ROS/src/PAM_User/src/user_package/resource/User_GUI.ui", self)
         self.setWindowTitle("패트와 매트")
         
@@ -32,23 +30,15 @@ class Ros2PyQtApp(QDialog):
         self.update_image_signal.connect(self.update_map_label)
 
     def update_map_label(self, map_image):
-        # numpy 배열을 QImage로 변환
         height, width = map_image.shape
         bytes_per_line = width
         q_image = QImage(map_image.data, width, height, bytes_per_line, QImage.Format.Format_Grayscale8)
-        
-        # QImage를 QPixmap으로 변환하여 QLabel에 설정
         pixmap = QPixmap.fromImage(q_image)
 
-
-        # QLabel의 크기 가져오기
         label_width = self.map_label.width()
         label_height = self.map_label.height()
 
-        # QPixmap을 QLabel 크기에 맞게 조정
         scaled_pixmap = pixmap.scaled(label_width, label_height, aspectRatioMode=QtCore.Qt.AspectRatioMode.KeepAspectRatio)
-
-        # QLabel에 이미지 설정
         self.map_label.setPixmap(scaled_pixmap)
 
 
@@ -77,87 +67,53 @@ class UserGUI(Node):
         self.map_image = cv2.imread('/home/hj/amr_ws/ROS/src/PAM_Admin/map.pgm', cv2.IMREAD_GRAYSCALE)
 
         # 맵 정보 설정
-        self.resolution = 0.05  # 맵의 해상도
-        self.origin = [-0.327, -1.71, 0]  # 맵의 원점
+        self.resolution = 0.05
+        self.origin = [-0.327, -1.71, 0]
 
-        # 이전 로봇 위치
         self.prev_robot_pose = None
-
-        # 초기 로봇 위치 설정
         self.robot_pose_x = int(-self.origin[0] / self.resolution)
         self.robot_pose_y = int(-self.origin[1] / self.resolution)
 
         self.update_map_image()
 
-        # 주기적으로 위치를 업데이트하는 타이머 설정
         self.timer = self.create_timer(0.1, self.timer_callback)
     
 
     def play_tts(self, text):
-        # TTS 생성
+        tts_thread = threading.Thread(target=self._play_tts, args=(text,))
+        tts_thread.start()
+
+    def _play_tts(self, text):
         tts = gTTS(text, lang='ko')
-
-        # 임시 파일로 저장
         tts.save('temp.mp3')
-
-        # 재생
         pygame.mixer.music.load('temp.mp3')
         pygame.mixer.music.play()
 
-        # 재생이 완료될 때까지 대기
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
 
-        # 임시 파일 삭제
         os.remove('temp.mp3')
-
 
     def robot_command_callback(self, request, response):
         self.get_logger().info(f'Received command: {request.command}')
 
         if request.command == "human_detect":
-            self.get_logger().info('Processing human_detect')
-            response.success = True
-            response.message = 'human_detect'
-
-            self.play_tts('무엇을 도와드릴까요?')
-
+            self.process_command(response, 'human_detect', '무엇을 도와드릴까요?')
 
         elif request.command == "description":
-            self.get_logger().info('Processing description')
-            response.success = True
-            response.message = 'description'
-
-            self.play_tts('작품설명 하겠습니다 {} , 또 다른 도움이 필요하십니까?'.format(request.description))
+            self.process_command(response, 'description', f'작품설명 하겠습니다 {request.description}, 또 다른 도움이 필요하십니까?')
 
         elif request.command == "guide":
-            self.get_logger().info('Processing guide')
-            response.success = True
-            response.message = 'guide'
-
-            self.play_tts('{}로 길안내 시작하겠습니다'.format(request.description))
+            self.process_command(response, 'guide', f'{request.description}로 길안내 시작하겠습니다')
 
         elif request.command == "re":
-            self.get_logger().info('Processing re')
-            response.success = True
-            response.message = 're'
-
-            self.play_tts(request.description)
+            self.process_command(response, 're', request.description)
 
         elif request.command == "again":
-            self.get_logger().info('Processing again')
-            response.success = True
-            response.message = 'again'
-
-            self.play_tts('작품이름을 넣어서 다시 한번 말씀해주세요')
+            self.process_command(response, 'again', '작품이름을 넣어서 다시 한번 말씀해주세요')
 
         elif request.command == "comeback":
-            self.get_logger().info('Processing comeback')
-            response.success = True
-            response.message = 'comeback'
-
-            self.play_tts('편안한 관람되십시오')
-
+            self.process_command(response, 'comeback', '편안한 관람되십시오')
 
         else:
             self.get_logger().error('Unknown command received')
@@ -165,37 +121,30 @@ class UserGUI(Node):
             response.message = 'Unknown'
 
         return response
+
+    def process_command(self, response, message, tts_text):
+        self.get_logger().info(f'Processing {message}')
+        response.success = True
+        response.message = message
+        self.play_tts(tts_text)
     
-
     def pose_callback(self, msg):
-        # self.get_logger().info('Received /amcl_pose')
-
-        # 로봇의 위치 받아오기
-        self.robot_pose_x = int((msg.pose.pose.position.x - self.origin[0]) / self.resolution)  # x 위치
-        self.robot_pose_y = int((msg.pose.pose.position.y - self.origin[1]) / self.resolution)  # y 위치
+        self.robot_pose_x = int((msg.pose.pose.position.x - self.origin[0]) / self.resolution)
+        self.robot_pose_y = int((msg.pose.pose.position.y - self.origin[1]) / self.resolution)
 
     def update_map_image(self):
-        # 맵 이미지의 높이
         map_height = self.map_image.shape[0]
 
-        # 이전 로봇 위치 지우기
         if self.prev_robot_pose is not None:
             cv2.circle(self.map_image, self.prev_robot_pose, 3, (255, 255, 255), -1)
 
-        # 로봇 위치를 이미지 좌표로 변환 (상하 반전)
         robot_image_y = map_height - self.robot_pose_y
-
-        # 맵 이미지에 새로운 로봇 위치 표시
         cv2.circle(self.map_image, (self.robot_pose_x, robot_image_y), 3, (0, 255, 0), -1)
-
-        # 이전 로봇 위치 업데이트
         self.prev_robot_pose = (self.robot_pose_x, robot_image_y)
 
-        # 신호를 통해 GUI 업데이트 요청
         self.gui_app.update_image_signal.emit(self.map_image)
 
     def timer_callback(self):
-        # 위치를 주기적으로 업데이트
         self.update_map_image()
 
 
@@ -228,3 +177,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
