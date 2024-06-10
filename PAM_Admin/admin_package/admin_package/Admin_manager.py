34b2ba43aae99b85ad24852f90bb42a996941e65
DJ_Y <djy404079@gmail.com>
2024-06-10 15:10:16 +0900

    Update Admin_manager.py
    작품 감지 시퀀스 수정

--- PAM_Admin/admin_package/admin_package/Admin_manager.py
+++ PAM_Admin/admin_package/admin_package/Admin_manager.py
@@ -54,6 +54,7 @@ class Admin_Manager(Node):
         
         self.arrive = False
         self.send_state = False   
+        self.night = False
         # 초기 이미지 생성 (예: 640x480, 검정색 이미지)
         initial_image = np.zeros((480, 640, 3), dtype=np.uint8)
         self.cv_img = initial_image     
@@ -175,7 +176,7 @@ class Admin_Manager(Node):
                     self.send_robot_command("human_detect")
                     self.send_state = True
                 else:
-                    if detect_art(self.art_model, self.cv_img):
+                    if self.night == True and detect_art(self.art_model, self.cv_img):
                         self.send_patrol_command("patrol")
                         self.send_state = True
                     else:
